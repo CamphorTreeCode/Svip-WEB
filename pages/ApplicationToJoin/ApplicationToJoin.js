a2d3b1cbe3bf71e83ce93b77d2663cf44248b25a
LXY <1140994323@qq.com>
2018-05-15 18:45:47 +0800

    页面基本信息

--- pages/ApplicationToJoin/ApplicationToJoin.js
+++ pages/ApplicationToJoin/ApplicationToJoin.js
@@ -1,70 +1,91 @@
 // pages/ApplicationToJoin/ApplicationToJoin.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    platform:'',
-   
+    platform: '',
+    franchiseDetailsImg: '',
+    franchiseDetailsContent: '',
+
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var franchiseDetails = this
+    var franchiser = this
+    //申请加盟页面信息
+    wx.request({
+      url: app.globalData.appUrl + 'WXCentre/findFranchiseDetails',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded', // 默认值
+        xcxuser_name: "xcxuser_name"
+      },
+      success: function (res) {
+        // console.info(res);
+        // console.info(res.data[0].franchisedetailscontent);
+        franchiseDetails.setData({
+          franchiseDetailsContent: res.data[0].franchisedetailscontent,
+          franchiseDetailsImg: res.data[0].franchisedetailsimg,
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
-  choosePlatform:function(){
+  choosePlatform: function () {
     wx.navigateTo({
       url: '/pages/platform/platform',
     })
@@ -74,6 +95,5 @@ Page({
   formSubmit: function (e) {
     console.info('触发事件')
     console.info(e)
-
   },
 })
