3117373f56233be830fe4a2b8d61806bb9611cd6
LXY <1140994323@qq.com>
2018-05-17 18:54:41 +0800

    调整格式

--- pages/study/study.js
+++ pages/study/study.js
@@ -7,8 +7,8 @@ Page({
    */
   data: {
 
-      top:[],
-      },
+    top: [],
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -22,10 +22,10 @@ Page({
         'content-type': 'application/x-www-form-urlencoded', // 默认值
         xcxuser_name: "xcxuser_name"
       },
-      
+
       success: function (res) {
-        var toplist=[]
-        for(var i=0;i<res.data.length;i++){
+        var toplist = []
+        for (var i = 0; i < res.data.length; i++) {
           toplist.push(res.data[i])
         }
         top.setData({
@@ -33,60 +33,61 @@ Page({
 
         })
         console.info(top.data.top)
-      } 
+      }
     })
-    
+
   },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   },
-  lujing:function(){
+  lujing: function () {
     wx.navigateTo({
       url: '/pages/study/studydetail/studydetail'
     })
