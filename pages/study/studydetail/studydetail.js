dc21af6bb2757c0ba98e9a419002b8b2254a8b96
LiChengLong <6025242704@qq.com>
2018-05-18 10:13:48 +0800

    提交

--- pages/study/studydetail/studydetail.js
+++ pages/study/studydetail/studydetail.js
@@ -1,23 +1,12 @@
+
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    vedio: "/img/study/movie.ogg",
-    zan:"https://www.chuanshoucs.com/ServerImg/2018-05-11/2c71c135-0189-4520-9bbf-77ebac0b8648.png",
-    fenxiang:"https://www.chuanshoucs.com/ServerImg/2018-05-11/3604a53a-7842-490e-8888-20030d79ccdc.png",
-    top: [
-      { image: "https://www.chuanshoucs.com/ServerImg/2018-05-11/705e081f-a6fb-4bcd-9711-8f1a1092a29d.png", 
-        time: "2018-04-17 14:34", 
-        name: "淘宝推广教程", 
-        detail: "在西方经济学中,分销的含义是建立在销售渠道d额意思,根据著名的营销大师菲利普·科勒特的定义，分销渠道(Distribution Channel)又或者叫营销渠道(Marketing Channel),是指某种商品(Commodity)或服务(Service)从生产者(Producer)向消费者(Consumer)转移的过程中，取得这种商品，服务所有权帮助所有权转移所有企业和个人",  
-        yan: "https://www.chuanshoucs.com/ServerImg/2018-05-11/32c140a8-fa11-4179-837e-a406e56ecfd5.png", 
-        eys: "129", 
-        zan: "78", 
-        fabulous: "https://www.chuanshoucs.com/ServerImg/2018-05-11/2c71c135-0189-4520-9bbf-77ebac0b8648.png" }
-
-    ],
+    top: [{ }],
     
   },
 
@@ -25,7 +14,18 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var top = this;
+    wx.request({
+      url: app.globalData.appUrl + 'WXLearning/findLearningDetails',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded', // 默认值
+        xcxuser_name: "xcxuser_name"
+      },
+      
+      success: function (res) {
+        
+      }
+    })
   },
 
   /**
